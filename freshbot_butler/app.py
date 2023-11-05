e30e79950d852ffabe49b654c5beed28cc90b473
Johannes Hackbarth <j.hackbarth@cubegaming.de>
2023-11-05 19:45:59 +0100

    Add simple database queries with langchain qa

--- freshbot_butler/app.py
+++ freshbot_butler/app.py
@@ -1,7 +1,24 @@
+import asyncio
+
 from bson.objectid import ObjectId
 from datetime import datetime, timedelta
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, jsonify, request, render_template, redirect, url_for
 from pymongo import MongoClient
+
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import MongodbLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.vectorstores import Chroma
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017/')
@@ -19,6 +36,51 @@ def get_expired_items():
         elif expiration_date <= datetime.now() + timedelta(days=3):
             expiring_items.append(item)
     return expired_items, expiring_items
+
+def generate_summary(items):
+    names = [item['name'] for item in items]
+    quantities = [item['quantity'] for item in items]
+    total_quantity = sum(quantities)
+    summary = f"The items in the collection are: {', '.join(names)}. The total quantity is {total_quantity}."
+    return summary
+
+async def get_response(query: str):
+    mongo_loader = MongodbLoader('mongodb://localhost:27017/', 'pantry', 'items')
+    docs = await mongo_loader.aload()
+    embeddings = OpenAIEmbeddings()
+    docsearch = Chroma.from_documents(docs, embeddings)
+
+    qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name="gpt-4", temperature=0.2), 
+                                    chain_type="stuff",
+                                    retriever=docsearch.as_retriever(),
+                                    return_source_documents=True)
+
+    prompt_template = PromptTemplate.from_template("{query} Heute ist der {date}, falls das Ablaufdatum vor dem heutigen Tag liegt, ist das Objekt nicht mehr haltbar. Das Datumsformat ist TT.MM.JJJJ.")
+
+    date = datetime.now().strftime('%d.%m.%Y')
+
+    query = prompt_template.format(date=date, query=query)
+    response = qa(query)
+    return response
+
+@app.route('/langchain', methods=['GET', 'POST'])
+def langchain_query():
+    if request.method == 'POST':
+        data = request.get_json()
+        query = data['query']
+        items = list(collection.find())
+
+        if query == 'summary':
+            summary = generate_summary(items)
+            response = f"There are {len(items)} items in the collection. {summary}"
+        else:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            res = loop.run_until_complete(get_response(query))
+            response = res["result"]
+        return jsonify({'response': response})
+    else:
+        return render_template('langchain.html')
 
 @app.route('/')
 def index():
